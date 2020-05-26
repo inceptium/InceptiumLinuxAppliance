96d03f2ef13c5df1a64dd225bb0405eb2d32c8a1
Antonio <cpmatrix@mac.com>
2020-05-26 18:19:28 +0200

    Update Install

---
+++ InceptiumServices/install_inceptium.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /root/InceptiumLinuxAppliance/InceptiumServices/
+INCEPTIUM_ACTIVE=1
+LOGFILE=/root/IncStorage/log/inceptium_srv.log
+DATASTORAGE=/root/IncStorage
+APPSTORE=/root/IncStorage/InceptiumAppStore
+MSG=0
+RESTARTCOUNT=0
+rm $LOGFILE
+
+	java -Xms128m -Xmx256m -cp "InceptiumServices.jar:/root/InceptiumLinuxAppliance/InceptiumServices/lib/*:/root/IncStorage/InceptiumAppStore/*" inceptiumserver.core.server.IncServer noswing updatedatabase landevice=eth0 datastorage=$DATASTORAGE appstore=$APPSTORE install
