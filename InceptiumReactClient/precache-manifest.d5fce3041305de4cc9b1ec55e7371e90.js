ac7e8d84d0b83a6d5072870e1f5c7501c1482636
Antonio <cpmatrix@mac.com>
2021-02-25 10:42:54 +0100

    UpdateClientReact

---
+++ InceptiumReactClient/precache-manifest.d5fce3041305de4cc9b1ec55e7371e90.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b20fb8499c0b20d8c74000dd288db55",
+    "url": "/portaledef/index.html"
+  },
+  {
+    "revision": "d13935721940062720e2",
+    "url": "/portaledef/static/css/2.7c5c5277.chunk.css"
+  },
+  {
+    "revision": "6b23a1a8e22d0075dfda",
+    "url": "/portaledef/static/css/main.b7c40936.chunk.css"
+  },
+  {
+    "revision": "d13935721940062720e2",
+    "url": "/portaledef/static/js/2.8c91cfe6.chunk.js"
+  },
+  {
+    "revision": "44f9dd15b7c08b104b0b71db6192b31e",
+    "url": "/portaledef/static/js/2.8c91cfe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b23a1a8e22d0075dfda",
+    "url": "/portaledef/static/js/main.b79bcc54.chunk.js"
+  },
+  {
+    "revision": "61ffe436f95d28546375",
+    "url": "/portaledef/static/js/runtime-main.f56531f5.js"
+  },
+  {
+    "revision": "8e9a9dd13b1723787ed1bf3cec6f7fd4",
+    "url": "/portaledef/static/media/app_icon.8e9a9dd1.png"
+  },
+  {
+    "revision": "c394e2399b6a7b854d1dedce6af46dcc",
+    "url": "/portaledef/static/media/logo.c394e239.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portaledef/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portaledef/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portaledef/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portaledef/static/media/slick.f97e3bbf.svg"
+  }
+]);
